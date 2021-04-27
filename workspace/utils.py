50090798b087121dcff5c0b05e2e0e9eb779dad9
ryuseunghwan1 <rechristmas58@gmail.com>
2021-04-27 16:47:56 +0900

    'change_feature_name'

--- workspace/utils.py
+++ workspace/utils.py
@@ -119,10 +119,10 @@ def split_train_test(df):
     train_set = df[df['test_set'] == 0]
     test_set = df[df['test_set'] == 1]
 
-    X_train = train_set.drop(['test_set','fraud_YN'], axis=1)
+    X_train = train_set.drop(['c_25','c_1'], axis=1)
     y_train = train_set['fraud_YN']
 
-    X_test = test_set.drop(['test_set', 'fraud_YN'], axis=1)
+    X_test = test_set.drop(['c_25', 'c_1'], axis=1)
     y_test = test_set['fraud_YN']
     
     print('==Split Result==')
